79661ea180f5390d68f0282958784f6f6978550d
Yunhong Min <yunhongmin00@gmail.com>
2026-02-26 15:51:39 +0900

    add linking context

---
+++ src/LinkingContext.cpp
@@ -0,0 +1,53 @@
+#include <cstdint>
+#include <unordered_map>
+#include "GameObject.cpp"
+
+class LinkingContext {
+    public:
+        LinkingContext(): mNextId(1) {}
+        uint32_t GetId(GameObject* inGameObject, bool shouldCreateIfNotFound) {
+            auto it = mGameObjectToIdMap.find(inGameObject);
+            if (it != mGameObjectToIdMap.end()) {
+                return it->second;
+            }
+            if (shouldCreateIfNotFound) {
+                uint32_t newId = GetNextId();
+                mGameObjectToIdMap[inGameObject] = newId;
+                mIdToGameObjectMap[newId] = inGameObject;
+                return newId;
+            }
+            return 0; // 0 can be reserved for "not found"
+        }
+
+        void AddGameObject(uint32_t inId, GameObject* inGameObject) {
+            if (inId == 0) {
+                throw std::invalid_argument("ID 0 is reserved for 'not found'");
+            }
+
+            if (mIdToGameObjectMap.find(inId) != mIdToGameObjectMap.end()) {
+                throw std::runtime_error("ID already exists in the linking context");
+            }
+
+            if (mGameObjectToIdMap.find(inGameObject) != mGameObjectToIdMap.end()) {
+                throw std::runtime_error("GameObject already exists in the linking context");
+            }
+
+            mIdToGameObjectMap[inId] = inGameObject;
+            mGameObjectToIdMap[inGameObject] = inId;
+        }
+
+        void RemoveGameObject(GameObject* inGameObject) {
+            auto it = mGameObjectToIdMap.find(inGameObject);
+            if (it != mGameObjectToIdMap.end()) {
+                uint32_t id = it->second;
+                mGameObjectToIdMap.erase(it);
+                mIdToGameObjectMap.erase(id);
+            }
+        }
+    private:
+        uint32_t mNextId;
+        uint32_t GetNextId() { return mNextId++; }
+        std::unordered_map<GameObject*, uint32_t> mGameObjectToIdMap;
+        std::unordered_map<uint32_t, GameObject*> mIdToGameObjectMap;
+};
+
