ee3d6502c4153f627a1f46828f9b2fdadad32d7a
Yunhong Min <yunhongmin00@gmail.com>
2026-02-26 19:42:02 +0900

    bits bug fix

--- src/InputMemoryBitStream.cpp
+++ src/InputMemoryBitStream.cpp
@@ -46,24 +46,47 @@ class InputMemoryBitStream {
 };
 
 void InputMemoryBitStream::ReadBits(uint8_t& outData, size_t inBitCount) {
+    // uint32_t byteOffset = mBitHead >> 3;
+	// uint32_t bitOffset = mBitHead & 0x7;
+	
+	// outData = static_cast< uint8_t >( mBuffer[ byteOffset ] ) >> bitOffset;
+	
+	// uint32_t bitsFreeThisByte = 8 - bitOffset;
+
+    // // if the bits we need to read span across the current byte and the next byte, we need to read from the next byte as well
+	// if( bitsFreeThisByte < inBitCount )
+	// {
+	// 	//we need another byte
+	// 	outData |= static_cast< uint8_t >( mBuffer[ byteOffset + 1 ] ) << bitsFreeThisByte;
+	// }
+	
+	// //don't forget a mask so that we only read the bit we wanted...
+	// outData &= ( ~( 0x00ff << inBitCount ) );
+	
+	// mBitHead += inBitCount;
     uint32_t thresholdBitCapacity = mBitHead + static_cast<uint32_t>(inBitCount);
     if (thresholdBitCapacity > mBitCapacity) {
         throw std::runtime_error("InputMemoryBitStream::Read - not enough data to read");
     }
 
+
     uint32_t byteOffset = mBitHead >> 3;
     uint32_t bitOffset = mBitHead & 0x07;
     uint32_t leftBitsInCurrentByte = 8 - bitOffset;
+    
+    // mBuffer is char* which is shifted with keeping signedness on some platforms, so we need to cast it to uint8_t to avoid sign extension when shifting.
+    // <Example>
+    // Unsigned shift: 0b10101000 (-88 in signed char) >> 2 = 0b00101010 (+42 in unsigned char)
+    // Signed shift: 0b10101000 >> 2 (-88 in signed char) = 0b11101010 (-22 in signed char, which is not what we want)
+    outData = static_cast<uint8_t>(mBuffer[byteOffset]) >> bitOffset;
 
-    if (leftBitsInCurrentByte >= inBitCount) {
-        outData = (mBuffer[byteOffset] >> bitOffset) & ((1 << inBitCount) - 1);
-    } else {
-        outData = (mBuffer[byteOffset] >> bitOffset);
-        inBitCount -= leftBitsInCurrentByte;
-        // inBitCount < 8 is guaranteed here, so we can safely read the next byte
-        outData |= (mBuffer[byteOffset + 1] & ((1 << inBitCount) - 1)) << leftBitsInCurrentByte;
+    if (leftBitsInCurrentByte < inBitCount) {
+        outData |= static_cast<uint8_t>(mBuffer[byteOffset + 1]) << leftBitsInCurrentByte;
     }
-        mBitHead = thresholdBitCapacity;
+
+    // Mask out the bits we don't care about (for inBitCount less than 8)
+	outData &= ( ~( 0x00ff << inBitCount ) );
+    mBitHead = thresholdBitCapacity;
 }
 
 
